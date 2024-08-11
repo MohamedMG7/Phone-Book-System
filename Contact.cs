3520a962d7cbf54cbf7ba20cbff244826022e32a
MohamedMG7 <mahamedabd096@gmail.com>
2024-08-11 21:51:27 +0300

    project intialization and contact class

---
+++ Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace phone_book_system
+{
+	internal class Contact
+	{
+		string Name { set; get; }
+		string Phone_Number { set; get; }
+		string Second_Phone_Number { set; get; }
+		bool IsFavourite { set; get; } 
+
+
+	}
+}
